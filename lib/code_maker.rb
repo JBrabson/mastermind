294e44a25e9733d40f9295ba43bc67c53c1623a8
koshea9 <keegan.oshea9@gmail.com>
2021-02-14 13:08:38 -0700

    Refactor Class

--- lib/code_maker.rb
+++ lib/code_maker.rb
@@ -1,21 +1,19 @@
 class CodeMaker
 
-  attr_reader :colors,
-              :number_of_positions,
-              :secret_code
+  attr_reader :secret_code
 
-  def initialize(colors, number_of_positions)
+  def initialize
     @colors = ["r", "y", "g", "b"]
     @number_of_positions = 4
     @secret_code = []
   end
 
-  def create_code_list(number_of_positions)
-    @colors.repeated_permutation(number_of_positions).to_a
+  def create_code_list
+    @colors.repeated_permutation(@number_of_positions).to_a
   end
 
   def secret_code
-    @secret_code = create_code_list(number_of_positions).sample
+    @secret_code = create_code_list.sample
   end
 
 end
