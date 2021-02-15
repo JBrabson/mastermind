e54ed84bc3880d4b1941376d0dee7eff13650636
koshea9 <keegan.oshea9@gmail.com>
2021-02-14 20:12:38 -0700

    Add logic for play

--- lib/game.rb
+++ lib/game.rb
@@ -1,18 +1,18 @@
 require './lib/code_maker'
-# require './lib/codebreaker'
-require './lib/evaluator'
-require './lib/guess'
 require './lib/messages'
 require './lib/turn'
 
 class Game
   attr_accessor :secret_code
   attr_reader :colors,
-              :number_of_positions
+              :number_of_positions,
+              :guess
 
   def initialize
     @message = Messages.new
-    @secret = CodeMaker.new.secret_code
+    @secret_code = CodeMaker.new.secret_code
+    @guess_count = 0
+    @start_time = Time.now
   end
 
   def start
@@ -44,25 +44,26 @@ class Game
   def play
     @message.play
     guess = get_user_input
-    evalguess = Guess.new(guess)
-    # require 'pry'; binding.pry
-      if evalguess.valid?
-        turn = Turn.new(guess, @secret)
+    @turn = Turn.new(guess, @secret_code)
+
+    if @turn.valid?
+      @guess_count +=1
+      require "pry"; binding.pry
+      @turn.result
+
+    elsif
+        @turn.characters_valid? == false
+        @message.invalid_characters
+      elsif
+        @turn.too_short? == true
+        @message.too_short
       else
-        if evalguess.characters_valid? == false
-          @message.invalid_characters
-        else evalguess.length_valid? == false
-          if evalguess.guess_array.count < 4
-            @message.too_short
-          else evalguess.guess_array.count > 4
-            @message.too_long
-          end
-        end
+        @turn.too_long? == true
+        @message.too_long
       end
-  end
+    end
 
   def get_user_input
     gets.chomp.downcase.delete(" ").split("")
   end
-
 end
