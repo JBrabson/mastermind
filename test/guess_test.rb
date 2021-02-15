d27805369f7053d9d90e36559bd6e552c2a98a5d
koshea9 <keegan.oshea9@gmail.com>
2021-02-14 20:59:55 -0700

    Fix tests

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -3,38 +3,6 @@ require 'minitest/pride'
 require './lib/guess'
 
 class GuessTest < Minitest::Test
-  def test_it_exists_and_has_attributes
-    guess = Guess.new(["r", "g", "b", "y"])
-    assert_instance_of Guess, guess
-    assert_equal ["r", "g", "b", "y"], guess.guess_array
-  end
 
-  def test_only_contains_allowed_characters
-    guess1 = Guess.new(["r", "g", "b", "y"])
-    guess2 = Guess.new(["c", "d", "e", "f"])
-    guess3 = Guess.new(["1", "d", "3", "d"])
-    assert_equal true, guess1.characters_valid?
-    assert_equal false, guess2.characters_valid?
-    assert_equal false, guess3.characters_valid?
-  end
-
-  def test_is_correct_length
-    #add messages
-    guess1 = Guess.new(["r", "g", "b", "y"])
-    guess2 = Guess.new(["r", "g", "b"])
-    guess3 = Guess.new(["r", "g", "b", "y", "r", "g", "b", "y"])
-    assert_equal 4, guess1.guess_array.length
-    assert_equal true, guess1.length_valid?
-    assert_equal false, guess2.length_valid?
-    assert_equal false, guess3.length_valid?
-  end
-
-  def test_guess_is_valid
-    guess1 = Guess.new(["r", "g", "b", "y"])
-    guess2 = Guess.new(["r", "g", "b"])
-    guess3 = Guess.new(["r", "g", "b", "y", "r", "g", "b", "y"])
-    assert_equal true, guess1.valid?
-    assert_equal false, guess2.valid?
-    assert_equal false, guess3.valid?
   end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -2,42 +2,58 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/code_maker'
-require './lib/evaluator'
 require './lib/turn'
 
 class TurnTest < Minitest::Test
 
-  def setup
-    @turn = Turn.new(@evaluator)
-    @evaluator = Evaluator.new(@secret_code, @guess)
-    @secret_code = ["r", "g", "b", "y"]
-    @guess = ["r", "y", "g", "b"]
+  def test_it_exists
+    turn = Turn.new(["g", "r", "r", "b"], ["r", "r", "g", "b"])
+    assert_instance_of Turn, turn
   end
 
-  def test_it_exists_and_has_attributes
-    assert_instance_of Turn, @turn
-    assert_equal ["r", "g", "b", "y"], @turn.evaluator.secret_code
-    assert_equal ["r", "y", "g", "b"], @turn.evaluator.guess
+  def test_only_contains_allowed_characters
+    turn1 = Turn.new(["r", "g", "b", "y"], ["r", "r", "g", "b"])
+    turn2 = Turn.new(["c", "d", "e", "f"], ["r", "r", "g", "b"])
+    turn3 = Turn.new(["1", "d", "3", "d"], ["r", "r", "g", "b"])
+    assert_equal true, turn1.characters_valid?
+    assert_equal false, turn2.characters_valid?
+    assert_equal false, turn3.characters_valid?
   end
 
-    #should new turns have both guesses or evaluator only?
-  #   @new = Turn.new(["r", "g", "b", "y"],
-  #                   ["r", "y", "g", "b"],
-  #                    @evaluator)
-  #   @evaluator = Evaluator.new
-  # end
-  #
-  # def test_it_exists_and_has_attributes
-  #   assert_instance_of Turn, @new
-  #   assert_equal ["r", "g", "b", "y"], @new.guess
-  #   assert_equal ["r", "y", "g", "b"], @new.secret_code
-  # end
-  #
-  # def test_it_can_have_evaluated_matches
-  #   assert_equal 2, @new.red_peg_exact_match
-  #
-  #
-  # end
-  #
-  # # def count
+  def test_is_correct_length
+    #add messages
+    turn1 = Turn.new(["r", "g", "b", "y"], ["r", "r", "g", "b"])
+    turn2 = Turn.new(["r", "g", "b"], ["r", "r", "g", "b"])
+    turn3 = Turn.new(["r", "g", "b", "y"], ["r", "g", "b", "y"])
+    assert_equal 4, turn1.guess.length
+    assert_equal true, turn1.length_valid?
+    assert_equal false, turn2.length_valid?
+    assert_equal false, turn3.length_valid?
+  end
+
+  def test_turn_is_valid
+    turn1 = Turn.new(["r", "g", "b", "y"], ["r", "r", "g", "b"])
+    turn2 = Turn.new(["r", "g", "b"], ["r", "r", "g", "b"])
+    turn3 = Turn.new(["r", "g", "b", "y"], ["r", "g", "b", "y"])
+    assert_equal true, turn1.valid?
+    assert_equal false, turn2.valid?
+    assert_equal false, turn3.valid?
+  end
+
+  def test_it_can_evaluate_right_color_right_place_exact_match
+  # skip
+  turn = Turn.new(["g", "r", "r", "b"], ["r", "r", "g", "b"])
+  secret_code = ["g", "r", "r", "b"]
+  guess =       ["r", "r", "g", "b"]
+
+  assert_equal 2, turn.correct_color_correct_location_count(secret_code, guess)
+  end
+
+  def test_it_can_evaluate_right_color_wrong_place_partial_match
+  # skip
+  turn = Turn.new(["g", "r", "r", "b"], ["r", "r", "g", "b"])
+  secret_code = ["g", "r", "r", "b"]
+  guess =       ["r", "r", "g", "b"]
+  assert_equal 3, turn.correct_color_count(secret_code, guess)
+  end
 end
